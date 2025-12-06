fb45100add535082e335cab61de2c053112b9664
Ckott <diegoangeles130703@gmail.com>
2025-12-06 02:49:39 -0500

    feat(wsaap): update WhatsApp avatar image URL

--- src/presentation/wsaap.tsx
+++ src/presentation/wsaap.tsx
@@ -6,7 +6,7 @@ export const Wsaap = () => {
     <FloatingWhatsApp
       accountName="Arena Roja"
       phoneNumber="+51902844989"
-      avatar="https://arenaroja.com/cdn/shop/files/Logo_Arena_Roja_250x.png?v=1689674863&width=360"
+      avatar="https://arenaroja.com/cdn/shop/files/Nueva_caja_2024_copia.jpg?v=1747438258&width=1280"
       statusMessage="Generalmente responde en minutos"
       chatMessage="Hola! ¿En qué podemos ayudarte?"
       placeholder="Escribe un Mensaje"
