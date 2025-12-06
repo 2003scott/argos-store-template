173f22b113820e6c7e4393dee491e1eb27152b1d
Ckott <diegoangeles130703@gmail.com>
2025-12-06 02:08:20 -0500

    feat(promotion): update layout and styling; adjust grid structure and button text

--- src/presentation/promotion.tsx
+++ src/presentation/promotion.tsx
@@ -1,4 +1,5 @@
 import { Button } from '@/components/ui/button';
+import { GrShop } from 'react-icons/gr';
 
 export const Promotion = () => {
   return (
@@ -7,7 +8,7 @@ export const Promotion = () => {
       <p className="text-center text-sm font-light uppercase">
         Aprovecha nuestra oferta exclusiva por tiempo limitado.
       </p>
-      <div className="grid grid-cols-3 gap-4">
+      <div className="grid grid-cols-2 gap-4 md:grid-cols-4">
         <img
           src="https://arenaroja.com/cdn/shop/files/IMG_7468_copia.jpg?v=1758560841&width=1024"
           alt="promotion-product"
@@ -18,19 +19,21 @@ export const Promotion = () => {
           alt="promotion-product"
           className="mt-4 w-full rounded-md"
         />
-        <div className="flex flex-col">
-          <img
-            src="https://arenaroja.com/cdn/shop/files/viajering_331393a5-57e7-44be-aeb2-06b2efaf0286.jpg?v=1755360841&width=360"
-            alt="product-promotion"
-            className="mt-4 w-full rounded-md"
-          />
-          <img
-            src="https://arenaroja.com/cdn/shop/files/Collaraceitunas.jpg?v=1758184713&width=360"
-            alt="product-promotion"
-            className="mt-4 w-full rounded-md"
-          />
-          <Button className="mt-4">Comprar Ahora</Button>
-        </div>
+        <img
+          src="https://arenaroja.com/cdn/shop/files/viajering_331393a5-57e7-44be-aeb2-06b2efaf0286.jpg?v=1755360841&width=360"
+          alt="product-promotion"
+          className="mt-4 w-full rounded-md"
+        />
+        <img
+          src="https://arenaroja.com/cdn/shop/files/Collaraceitunas.jpg?v=1758184713&width=360"
+          alt="product-promotion"
+          className="mt-4 w-full rounded-md"
+        />
+      </div>
+      <div className="flex justify-center">
+        <Button size={'lg'} className="mt-4">
+          Comprar Promocion <GrShop />
+        </Button>
       </div>
     </div>
   );
